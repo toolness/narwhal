fa424aea13754b55e614e3ce2d4c1ed970e5fec7
Atul Varma <varmaa@host-5-148.mv.mozilla.com>
2009-09-08 16:26:58 -0700

    Added bootstrap js script and moved some logic from narwhal.py to it.

--- engines/pydermonkey/python-lib/narwhal.py
+++ engines/pydermonkey/python-lib/narwhal.py
@@ -5,78 +5,69 @@ import traceback
 import pydermonkey
 from pydershell import JsSandbox, JsExposedObject, jsexposed
 
+class PyderApi(JsExposedObject):
+    __jsprops__ = ['info']
+
+    def __init__(self, runner):
+        self._runner = runner
+        self._sandbox = runner.sandbox
+        self._root_dir = runner.home_dir
+
+    def _real_path(self, path):
+        path = self._root_dir + path
+        path = os.path.normpath(path)
+        path = os.path.realpath(path)
+        if not path.startswith(self._root_dir):
+            return None
+        return path
+
+    @property
+    def info(self):
+        return self._sandbox.new_object(
+            os = sys.platform
+            );
+
+    @jsexposed
+    def read(self, filename, args=None):
+        path = self._real_path(filename)
+        if not path:
+            raise pydermonkey.error("invalid filename: %s" % filename)
+        try:
+            contents = open(path).read()
+        except IOError, e:
+            raise pydermonkey.error(str(e))
+        if args and args.charset:
+            contents = contents.decode(args.charset)
+        return contents
+
+    @jsexposed
+    def isFile(self, filename):
+        path = self._real_path(filename)
+        if not path:
+            return False
+        return os.path.isfile(path)
+
+    @jsexposed
+    def printString(self, *args):
+        print " ".join(args)
+
+    @jsexposed
+    def evaluate(self, code, filename='<string>', lineno=1):
+        if filename != '<string>':
+            filename = self._root_dir + filename
+        return self._sandbox.evaluate(code, filename, lineno)
+
 class NarwhalRunner(object):
     def __init__(self, argv, home_dir, engine_home_dir):
         self.argv = argv
         self.home_dir = home_dir
         self.engine_home_dir = engine_home_dir
         self.sandbox = JsSandbox()
-        self.sandbox.root['global'] = pydermonkey.undefined
+        self.sandbox.root.pyder = PyderApi(self)
 
     def run(self):
-        filename = os.path.join(self.home_dir, 'narwhal.js')
-        return self.sandbox.run_script(filename,
-                                       self.__call_narwhal)
-
-    def _real_path(self, path):
-        path = self.home_dir + path
-        path = os.path.normpath(path)
-        path = os.path.realpath(path)
-        if not path.startswith(self.home_dir):
-            return None
-        return path
-
-    def __make_system_object(self):
-        fs = self.sandbox.new_object()
-
-        @jsexposed(on=fs)
-        def read(filename, args=None):
-            path = self._real_path(filename)
-            if not path:
-                raise pydermonkey.error("invalid filename: %s" % filename)
-            contents = open(path).read()
-            if args and args.charset:
-                contents = contents.decode(args.charset)
-            return contents
-
-        @jsexposed(on=fs)
-        def isFile(filename):
-            path = self._real_path(filename)
-            if not path:
-                return False
-            return os.path.isfile(path)
-
-        system = self.sandbox.new_object(
-            engine = 'pydermonkey',
-            engines = self.sandbox.new_array('pydermonkey', 'default'),
-            os = sys.platform,
-            prefix = '',
-            prefixes = self.sandbox.new_array(''),
-            debug = False,
-            verbose = False,
-            fs = fs
-            )
-        system['global'] = self.sandbox.root
-
-        @jsexposed(on=system, name='print')
-        def jsprint(*args):
-            print args
-
-        @jsexposed(on=system)
-        def evalGlobal(*args):
-            raise NotImplementedError("evalGlobal not implemented")
-
-        @jsexposed(on=system)
-        def evaluate(code, filename='<string>', lineno=1):
-            filename = self.home_dir + filename
-            code = ("function(require,exports,module,system,print){"
-                    "%s\n// */\n}" % code)
-            return self.sandbox.evaluate(code, filename, lineno)
-
-        return system
-
-    def __call_narwhal(self, narwhal):
-        narwhal(self.__make_system_object())
+        filename = os.path.join(self.engine_home_dir, 'bootstrap.js')
+        return self.sandbox.run_script(filename)
 
 def run(*args, **kwargs):
     runner = NarwhalRunner(*args, **kwargs)
